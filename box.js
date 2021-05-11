3982fccf28cff8ba529b23f55c007003470864b3
anna-15 <79515389+anna-15@users.noreply.github.com>
2021-05-11 13:19:22 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,28 @@
+class Box{
+    constructor(x,y,width,height){
+       var options ={
+         friction:1.0,
+         density:0.04,
+         restitution:0.8
+           
+       }
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+
+    World.add(world,this.body);
+    
+    }
+  display(){
+ var angle = this.body.angle;
+ var position =this.body.position;
+  push();
+  translate(position.x,position.y);
+  rotate(angle);
+  rectMode(CENTER);
+  strokeWeight(4);
+  stroke("green");
+  rect(0,0,this.width,this.height);
+  pop();
+  }
+}
